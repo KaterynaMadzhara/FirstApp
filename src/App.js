beaefbd5321387fa472180bfc89f29bbf44d1803
Kateryna Madzhara <katerynam@wix.com>
2022-07-14 16:25:05 +0300

    add my first app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
-import logo from './logo.svg';
+import logo from './img.png';
+import logo1 from './img_1.png';
+import logo2 from './img_2.png';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [isActive, setIsActive] = useState(false);
+
+    const handleClick = () => {
+        setIsActive(!isActive);
+    };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className='hover-button'>
+            <button class='hover-button--off'>
+                <img src={logo} className="App-logo" alt="logo" />
+            </button>
+            <button className='hover-button--on' onClick={()=> handleClick()}>
+                <img src={isActive? logo2: logo1}  className="App-logo" alt="logo"/>
+            </button>
+        </div>
       </header>
     </div>
   );
